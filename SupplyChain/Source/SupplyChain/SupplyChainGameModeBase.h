18500bae8930ced5a948290df250a0f742fe2b4b
JoshAng <ja2310g@greenwich.ac.uk>
2021-11-10 15:24:13 +0000

    Test
    1st Test

---
+++ SupplyChain/Source/SupplyChain/SupplyChainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SupplyChainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPPLYCHAIN_API ASupplyChainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
